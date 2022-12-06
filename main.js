f95ed6ecdceca1ef60b5930a645b63b6ef417d69
lmorozco <lorozco@valizant.com>
2022-12-05 17:24:15 -0800

    Ventajas de la POO

--- main.js
+++ main.js
@@ -58,3 +58,115 @@ const miguel = new Student2({
     name: "Miguel",
     age: 28,
 });
+
+//Ventajas de la POO en JS
+/*
+const juan1 = {
+    name: "JuanDC",
+    username: "juandc",
+    points: 100,
+    socialMedia: {
+        twitter: "fjuandc",
+        instagram: "fjuandc",
+        facebook: "undefined",
+    },
+    approvedCourses: [
+        "Curso definitivo de HTML y CSS",
+        "Curso practico de HTML y CSS",
+    ],
+    learningPaths: [
+        {
+            name: "Escuela de Desarrollo Web",
+            courses: [
+                "Curso definitivo de HTML y CSS",
+                "Curso practico de HTML y CSS",
+                "Curso de Responsive Design",
+            ],
+        },
+        {
+            name: "Escuela de Videojuegos",
+            courses: [
+                "Curso de Produccion",
+                "Curso de Unreal Engine",
+                "Curso de Unity 3D",
+            ],
+        },
+    ],
+};
+
+const miguelito1 = {
+    name: "Miguel",
+    username: "miguelito",
+    points: 100,
+    socialMedia: {
+        twitter: "miguelitoxd",
+        instagram: "miguelitoxd",
+        facebook: "miguelitoxd",
+    },
+    approvedCourses: [
+        "Curso Data Business",
+        "Curso Dataviz",
+    ],
+    learningPaths: [
+        {
+            name: "Escuela de Desarrollo Web",
+            courses: [
+                "Curso definitivo de HTML y CSS",
+                "Curso practico de HTML y CSS",
+                "Curso de Responsive Design",
+            ],
+        },
+        {
+            name: "Escuela de Data Science",
+            courses: [
+                "Curso Data Business",
+                "Curso Dataviz",
+                "Curso Tableau",
+            ],
+        },
+    ],
+};
+*/
+
+//Utilizar POO
+
+class Student {
+    constructor({
+        name,
+        email,
+        username,
+        twitter = undefined,
+        instagram = undefined,
+        facebook = undefined,
+        approvedCourses = [],
+        learningPaths = [],
+    }){
+        this.name = name;
+        this.email = email;
+        this.username = username;
+        this.socialMedia = {
+            twitter,
+            instagram,
+            facebook,
+        };
+        this.approvedCourses = approvedCourses;
+        this.learningPaths = learningPaths;
+    }
+}
+
+const juan2 = new Student({
+    name: "JuanDC",
+    username: "juandc",
+    email: "juanito@gmail.com",
+    twitter: "juanitodc",
+    instagram: "juanitodc",
+});
+
+const miguelito2 = new Student({
+    name: "Miguel",
+    username: "miguelito",
+    email: "miguelito@gmail.com",
+    twitter: "miguelitoxd",
+});
+
+//Reto Crear class Learningpaths, const escuelas (13:50 Clase 7)
